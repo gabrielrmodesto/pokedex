581edc185e5afb82d30a40710c7ddafe3ef79bd5
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-05-28 14:44:23 -0300

    Pokedex list project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './views/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
