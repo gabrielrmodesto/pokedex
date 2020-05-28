97da27c0ff10aa3055c43ce4d0028bb0208d6172
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-05-28 18:04:10 -0300

    List the pokemon

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+	selector: 'app-list',
+	templateUrl: './list.component.html',
+	styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+	pokemonList = [
+		{
+			name: 'Bulbasaur',
+			number: 1,
+		},
+		{
+			name: 'Charmander',
+			number: 4,
+		},
+		{
+			name: 'Squirtle',
+			number: 7,
+		},
+		{
+			name: 'Pikachu',
+			number: 25,
+		},
+	];
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
