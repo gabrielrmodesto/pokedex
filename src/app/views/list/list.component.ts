6cb6665def8a2b9e2dd0eb256327d005754bc0f6
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-05-29 16:20:47 -0300

    Make request to a service

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -1,3 +1,4 @@
+import { PokeapiService } from './../../services/pokeapi.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,34 +7,27 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+	nameFilter = '';
 	selectedPokemon = null;
-	pokemonList = [
-		{
-			name: 'Bulbasaur',
-			number: 1,
-		},
-		{
-			name: 'Charmander',
-			number: 4,
-		},
-		{
-			name: 'Squirtle',
-			number: 7,
-		},
-		{
-			name: 'Pikachu',
-			number: 25,
-		},
-	];
-	get pkmSprite(){
+	get pokemonList() {
+		return this.pokeList.pokeList.filter((pokemon) => {
+			return (
+				pokemon.name
+					.toLowerCase()
+					.indexOf(this.nameFilter.toLowerCase()) !== -1
+			);
+		});
+	}
+
+	get pkmSprite() {
 		const number = ('000' + this.selectedPokemon.number).slice(-3);
 		return `//serebii.net/pokedex-xy/icon/${number}.png`;
 	}
-	constructor() {}
+	constructor(private pokeList: PokeapiService) {}
 
 	ngOnInit(): void {}
 
-	selectPokemon(pkm){
+	selectPokemon(pkm) {
 		this.selectedPokemon = pkm;
 	}
 }
